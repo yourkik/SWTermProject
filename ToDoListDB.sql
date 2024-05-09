b61e01237a9ae349cbf2a250eff106fdf27b877b
yourkik <73531665+yourkik@users.noreply.github.com>
2024-05-09 18:34:22 +0900

    make basic storage
    make basic storage and setting foreign key

--- ToDoListDB.sql
+++ ToDoListDB.sql
@@ -1,4 +1,43 @@
 create database schedule;
 use schedule;
 
-select * from userTable;
+select * from users;
+
+CREATE TABLE tasks (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    detail TEXT,
+    priority INT,
+    status ENUM('Todo', 'In Progress', 'Done'),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+select * from tasks;
+
+CREATE TABLE daily_plans (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    date DATE,
+    task_id INT,
+    start_time TIME,
+    end_time TIME,
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (task_id) REFERENCES tasks(id)
+);
+
+CREATE TABLE project_plans (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    project_id INT,
+    description TEXT,
+    is_public BOOLEAN,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE notifications (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    message TEXT,
+    is_read BOOLEAN,
+    datetime DATETIME,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
