805d3ffec8536f2b57250ddc060982ea697c04ae
yourkik <73531665+yourkik@users.noreply.github.com>
2024-05-16 09:09:29 +0900

    Update ToDoListDB.sql
    User table 생성 코드를 sql에 넣었고 task에 시작 시간과 실행 시간을 저장하게 만들었습니다. 그리고 현재 사용하지 않고 사용성이 낮아보이는 table을 주석처리했습니다.

--- ToDoListDB.sql
+++ ToDoListDB.sql
@@ -3,16 +3,40 @@ use schedule;
 
 select * from users;
 
+create table users(
+	id int auto_increment,
+    password char(30),
+    email char(25),
+    phone_number char(15),
+    name char(15),
+    primary key(id, password)
+);
+
 CREATE TABLE tasks (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT,
     detail TEXT,
     priority INT,
-    status ENUM('Todo', 'In Progress', 'Done'),
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
+    startTime datetime,
+    excutionTime datetime,#여기에는 timedelta로 시간과 분만 들어감
+    status ENUM('Todo', 'In Progress', 'Done'),#현재 여기서 상태를 구분하지만 휴지통을 따로 만들거랴면 startTime과 excutionTime을 계산해서 다 끝난 task를 따로 저장할 수 있음
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
 select * from tasks;
 
+/*
+#task에서 따로 시간을 관리하는 table을 저장하는 ver, 시간의 변동이 많으면 task에 저장하는 것보다 효율적이라고 생각
+create table taskTime(
+	id int auto_increment primary key,
+    task_id int,
+    startTime datetime,
+    excutionTime datetime,
+    foreign key(task_id) references tasks(id)
+)
+*/
+
+/*
+불필요하다고 생각해 주석 처리함
 CREATE TABLE daily_plans (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT,
@@ -33,6 +57,7 @@ CREATE TABLE project_plans (
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
+
 CREATE TABLE notifications (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT,
@@ -41,3 +66,4 @@ CREATE TABLE notifications (
     datetime DATETIME,
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
+*/
